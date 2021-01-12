59d8c4a826652da868a5814f8cead619411813a3
Mishchenya <Mishchenya.igor@gmail.com>
2021-01-12 17:04:11 +0200

    initial commit

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+using namespace std;
+
+int main()
+{
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int arraySize = 100;
+    int array[arraySize][arraySize];
+    int sum_array[arraySize][arraySize];
+    int array_2[arraySize][arraySize];
+    int row;
+
+    cout << "Введіть кількість рядків та столбців матриць: ";
+    cin >> row;
+
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < row; ++j) {
+            cout << "Введіть A[" << i + 1 << "][" << j + 1 << "] значення: ";
+            cin >> array[i][j];
+        }
+    }
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < row; ++j) {
+            cout << "Введіть B[" << i + 1 << "][" << j + 1 << "] значення: ";
+            cin >> array_2[i][j];
+        }
+    }
+
+    cout << endl << "Вихідна матриця №1: " << endl;
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < row; ++j) {
+            cout << setw(5) << array[i][j];
+        }
+        cout << endl;
+    }
+    cout << endl << "Вихідна матриця №2: " << endl;
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < row; ++j) {
+            cout << setw(5) << array_2[i][j];
+        }
+        cout << endl;
+    }
+    cout << endl;
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < row; ++j)
+        {
+            sum_array[i][j] = array[i][j] + array_2[i][j];
+        }
+    }
+    cout << endl << "Вихідна матриця с суми матриці №1 та матриці №2: " << endl;
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < row; ++j) {
+            cout << setw(5) << sum_array[i][j];
+        }
+        cout << endl;
+    }
+    return 0;
+}
