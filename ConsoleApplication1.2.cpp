5fc5e907c42b6ebcd0528b5a9e640fadc2d50033
Mishchenya <Mishchenya.igor@gmail.com>
2021-01-12 17:13:02 +0200

    delete one cycle

--- ConsoleApplication1.2.cpp
+++ ConsoleApplication1.2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-
+	
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
@@ -46,18 +46,6 @@ int main()
         cout << endl;
     }
     cout << endl;
-    for (int i = 0; i < row; ++i) {
-        for (int j = 0; j < row; ++j)
-        {
-            sum_array[i][j] = array[i][j] + array_2[i][j];
-        }
-    }
-    cout << endl << "Вихідна матриця с суми матриці №1 та матриці №2: " << endl;
-    for (int i = 0; i < row; ++i) {
-        for (int j = 0; j < row; ++j) {
-            cout << setw(5) << sum_array[i][j];
-        }
-        cout << endl;
-    }
+
     return 0;
 }
